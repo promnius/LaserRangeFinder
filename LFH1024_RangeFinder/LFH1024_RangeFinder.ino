85eb43a29cd506ab6da69753d889affba7957bcd
promnius <promnius@users.noreply.github.com>
2020-07-30 22:52:17 -0500

    Implemented linear interpolation for distance calculation

--- LFH1024_RangeFinder/LFH1024_RangeFinder.ino
+++ LFH1024_RangeFinder/LFH1024_RangeFinder.ino
@@ -1,4 +1,7 @@
 
+// TODO: make mode variable persistent (and make calibration routine preserve it)
+// move all or most of math to fixed point (long) math, and/or make look up table faster
+// general speed boosts; reduction of unneeded math, timing stuff, etc.
 
 #include "GlobalVariables.h"
 #include "CCDRead.h"
@@ -32,7 +35,8 @@ void loop() {
   analogWrite(pinREALDAC, (int)(fltDistance*40.96)); // map 0-100 distance to 0-4096 for dac output
   if (intMode == 1){if (lngLoopCounter%100 == 0){plotCCDScan();}}
   else if (intMode == 2){if (lngLoopCounter%100 == 0){Serial.println(fltCentroidMath);}}
-  else if (intMode == 3){if (lngLoopCounter%100 == 0){Serial.println(fltDistance);}}
+  else if (intMode == 3){if (lngLoopCounter%100 == 0){Serial.println(fltDistance*5);}} //X5 is a hack to put it in real numbers- but only applies if this was calibrated
+  // to a 500 thou scale . . . probably should not be left like this
   else if (intMode == 8){if (lngLoopCounter%10000 == 0){plotCalibrationPoints();}}
 
   lngLoopCounter++;
--- LFH1024_RangeFinder/Math.h
+++ LFH1024_RangeFinder/Math.h
@@ -22,8 +22,23 @@ void computeCentroid(){
   }
 }
 
+// this could be faster, and may be a significant portion of wasted time. It's also
+// non-deterministic as it stands. It also doesn't handle the case where the input
+// is greater than the maximum value in the table. Distance will just be unassigned, so
+// it will stay as whatever it was from before. Maybe the calibration table needs
+// to be reconsidered, or the case for 'no signal detected' needs to be something other
+// than an output of 0 - or maybe the table needs to cal from 1-100 so 0 is reserved.
+// as it stands, the table is not even checked to know that it is continously increasing. 
 void computeDistance(){
-  
+  if (fltCentroidMath == 0){fltDistance = 0;} // no response
+  else if (fltCentroidMath < FLTcalibrationTable[0]){fltDistance = 0;} // response below scale . . . same as no response :/
+  else{
+    for (int i = 0; i < 99; i ++){
+      if (fltCentroidMath > FLTcalibrationTable[i] && fltCentroidMath < FLTcalibrationTable[i+1]){
+        fltDistance = (fltCentroidMath-FLTcalibrationTable[i])/(FLTcalibrationTable[i+1] - FLTcalibrationTable[i])+(float)(i);
+      }
+    }
+  }
 }
 
 #endif
